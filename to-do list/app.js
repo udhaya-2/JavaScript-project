7181ee6b5374fbe19e2685ea3ed6d7464ac83ac1
udhaya-2 <bhavanaramana8@gmail.com>
2024-11-22 21:40:02 -0800

    todo list

--- to-do list/app.js
+++ to-do list/app.js
@@ -1,6 +1,12 @@
 let input= document.querySelector('#input-box')
 let button = document.getElementById('adds')
+let completed=document.querySelector('.completed')
+let uncompleted=document.querySelector('.uncompleted')
+let complete=0
+let count=0
 let todos=[]
+
+
 // const read= new read()
 button.addEventListener('click',()=>{
     if(input.value.trim()==''){
@@ -8,10 +14,16 @@ button.addEventListener('click',()=>{
     }
     else{
         todos.push({text:input.value,completed:false})
+        count = todos.length
         addtodo(input.value)
         input.value=''}
 })
 function Delete(todo){
+        complete--
+        count--
+        if(complete <0)
+            complete=0
+        completed.innerHTML=complete
         if(todo >=0 && todo < todos.length){
             todos.splice(todo,1)
           }
@@ -19,6 +31,10 @@ function Delete(todo){
 }
 
 function addtodo(){
+    if (count<0)
+        uncompleted.innerHTML=0
+    else
+       uncompleted.innerHTML=count
     const tasklist= document.getElementById('Tasklist')
     tasklist.innerHTML=''
     todos.forEach((todo ,index)=>{
@@ -29,8 +45,10 @@ function addtodo(){
    })
    }
 function finish(index){
+      complete++
+      count--
+      completed.innerHTML=complete
       todos[index].completed = !todos[index].completed;
       addtodo();
-      
 }
 
